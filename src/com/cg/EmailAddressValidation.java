cd59db14c67b30b5f07e214a8304f6a9c21cb0ef
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-28 17:00:21 +0530

    Refactored: Email Address Validated

--- src/com/cg/EmailAddressValidation.java
+++ src/com/cg/EmailAddressValidation.java
@@ -5,7 +5,7 @@ import java.util.*;
 //UC5
 public class EmailAddressValidation {
 	public static void ValidateEmail(String input) {
-		String regex = "[A-Z 0-9]+[A-Z +._-]*[@][A-Z 0-9]+[.][A-Z]+[A-Z .]*$";
+		String regex = "^[A-Z 0-9]+[A-Z 0-9 +._-]*[@][A-Z 0-9]+[.][A-Z]+[A-Z .]*$";
 		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(input);
 		boolean found = matcher.find();
