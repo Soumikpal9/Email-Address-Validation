4312bbb9f13c5afe8bd820b7819eee46bac15907
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-28 16:27:55 +0530

    Added: Email Address First Part Validated

--- src/com/cg/EmailAddressValidation.java
+++ src/com/cg/EmailAddressValidation.java
@@ -1,7 +1,28 @@
 package com.cg;
+import java.util.regex.*;
+import java.util.*;
 
+//UC1
 public class EmailAddressValidation {
+	public static void ValidateEmail(String input) {
+		String regex = "[A-Z 0-9]+";
+		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(input);
+		boolean found = matcher.find();
+		if(found) {
+			System.out.println("Email Address Validated");
+		}
+		else {
+			System.out.println("Email Address Not Validated");
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Address Validation!!!");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter email address : ");
+		String email = sc.nextLine();
+		ValidateEmail(email);
 	}
 }
